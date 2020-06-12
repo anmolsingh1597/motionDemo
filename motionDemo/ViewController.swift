726864095ad43c87fe9619e68ed6ba914460f41a
Anmol Singh <anmolsingh1597@gmail.com>
2020-06-12 15:27:32 -0400

    [Anmol Singh]Function integrated

--- motionDemo/ViewController.swift
+++ motionDemo/ViewController.swift
@@ -10,10 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+  
+        
     }
+
+    func setUpwallpaper(){
+           let min = CGFloat(-100)
+           let max = CGFloat(100)
+           
+           //MARK: - apply motion to horizontal axis
+           let xMotion = UIInterpolatingMotionEffect(keyPath: "layer.transform.translation.x", type: .tiltAlongHorizontalAxis)
+           xMotion.minimumRelativeValue = min
+           xMotion.maximumRelativeValue = max
+           
+           //MARK: - apply motion to vertical axis
+           let yMotion = UIInterpolatingMotionEffect(keyPath: "layer.transform.translation.y", type: .tiltAlongVerticalAxis)
+           yMotion.minimumRelativeValue = min
+           yMotion.maximumRelativeValue = max
+           
+           let motionEffectGroup = UIMotionEffectGroup()
+           motionEffectGroup.motionEffects = [xMotion, yMotion]
+           
+           imageView.addMotionEffect(motionEffectGroup)
+           
+       }
 
 
 }
