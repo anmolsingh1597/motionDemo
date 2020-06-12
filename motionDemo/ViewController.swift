62cef0519e1546ba390406f4b5c5662e7382ed12
Anmol Singh <anmolsingh1597@gmail.com>
2020-06-12 15:05:45 -0400

    Initial Commit

---
+++ motionDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  motionDemo
+//
+//  Created by Anmol singh on 2020-06-12.
+//  Copyright © 2020 Swift Project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
